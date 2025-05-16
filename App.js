18020a460ca552a5bc581010779490d9d28f335b
leejh-23 <leejh.23@ichat.sp.edu.sg>
2025-05-16 22:40:25 +0800

    set up events stack

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
 import { createStaticNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 // Import local screens
 import HomeScreen from './screens/HomeScreen';
-import EventsScreen from './screens/EventsScreen';
+import EventsStack from './screens/EventsScreen';
 import ContactScreen from './screens/ContactScreen';
 
 // Navigator -----------------------------------------------
 const Tabs = createBottomTabNavigator({
-  screens: {
+  screens: { // associates the tabs with its relevant screen pages
     Home: HomeScreen,
-    Events: EventsScreen,
+
+    // to get rid of the double headers bc it looks a little funny
+    Events: {
+      screen: EventsStack,
+      options: { headerShown: false }
+    },
     Contact: ContactScreen,
   },
+  screenOptions: ({ route }) => ({
+    tabBarActiveTintColor: "red",
+    tabBarInactiveTintColor: "gray",
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      if (route.name === "Home") {
+        iconName = "home";
+      } else if (route.name === "Events") {
+        iconName = focused ? "calendar":"calendar-o";
+      } else { // if focused, show the filled address-book icon, else show the non-filled icon (? is just a shorthand for if statements)
+        iconName = focused ? "address-book": "address-book-o";
+      }
+
+      return <FontAwesome name={iconName} size={size} color={color} />;
+    }
+  })
 });
 
 // Root navigation component -------------------------------
-const Navigation = createStaticNavigation(Tabs);
+const Navigation = createStaticNavigation(Tabs); // creates the navigation bar with the specified tabs 
 
 export default function App() {
   return <Navigation />;
--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,4 +1,5 @@
 import {View, Text} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const EventsScreen = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -6,4 +7,19 @@ const EventsScreen = () => (
   </View>
 );
 
-export default EventsScreen;
+const Stack = createStackNavigator();
+
+// specify that u want the eventsstack to contain an eventsscreen
+const EventsStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Events"
+        component={EventsScreen}
+      />
+    </Stack.Navigator>
+  );
+};
+
+// stacks come with a header by default
+export default EventsStack;
