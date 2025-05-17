70a2fcae0fd948664326bf1d577aa2127de09a1d
leejh-23 <leejh.23@ichat.sp.edu.sg>
2025-05-17 13:43:40 +0800

    set up rn paper, home screen, contact screen

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,9 +1,38 @@
-import {View, Text} from "react-native";
+import { View, ScrollView } from "react-native";
+import { List, Avatar } from "react-native-paper";
+import contactsData from "../data.js";
 
+// scroll view has two parameters, style and contentcontainerstyle
 const ContactScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Contact!</Text>
-  </View>
-); 
+  <ScrollView style={{ backgroundColor: "#fff", flex: 1 }}>
+    <View>
+      {/* loops through the contactsData array and for each contact, print out the name and title */}
+      {/* current item is contact, index is just the index num we r currently at */}
+      {/* notice that there is a curly brace before the map function bc we are switching back to js */}
+
+      {contactsData.map((contact, idx) => (
+        <List.Item
+          key={contact.name + idx}
+          title={contact.name}
+          description={`${contact.title}, ${contact.company}`}
+          left={() => (
+            <Avatar.Image
+              size={48}
+              source={{ uri: contact.pic }}
+              style={{ marginTop: 8, marginLeft: 8 }}
+            />
+          )}
+          titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+          descriptionStyle={{ color: "#b0b0b0", fontSize: 15 }}
+          style={{
+            paddingVertical: 8,
+            borderBottomWidth: 1,
+            borderBottomColor: "#eee"
+          }}
+        />
+      ))}
+    </View>
+  </ScrollView>
+);
 
 export default ContactScreen;
